4488687280d5b6b60bbcc10160623787659d7f7e
TeetouchQQ <teawkung@live.com>
2022-09-20 13:45:39 +0700

    week6Final

--- main.cpp
+++ main.cpp
@@ -21,24 +21,34 @@ HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 CONSOLE_CURSOR_INFO lpCursor;
 lpCursor.bVisible = visible;
 lpCursor.dwSize = 20;
-SetConsoleCursorInfo(console,&lpCursor);a
+SetConsoleCursorInfo(console,&lpCursor);
 }
 
 void gotoxy(int x, int y) {
 	setcursor(0);
-	setcolor(5,0);
+	
 	COORD c = { x, y };
 	SetConsoleCursorPosition( GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
 void draw_ship(int x,int y) {
+	setcolor(2,4);
 	gotoxy(x,y);
+	printf(" <-0-> ");
+}
+void erase_ship(int x,int y) {
+	setcolor(0,0);
+	gotoxy(x,y);
+	printf("       ");
 }
 void draw_bullet(int x,int y) {
+	setcolor(1,0);
 	gotoxy(x,y);
+	printf("   |  ");
 }
 void clear_bullet(int x,int y) {
+	setcolor(0,0);
 	gotoxy(x,y);
-	printf(" ");
+	printf("       ");
 }
 
 int main() {
@@ -56,10 +66,12 @@ int main() {
 		if (_kbhit()) {
 			ch=_getch();
 			if(ch=='a') {
+				erase_ship(x,y);
 				x -= 1;
 				draw_ship(x,y);
 			}
 			if(ch=='d') {
+				erase_ship(x,y);
 				x+=1;
 				draw_ship(x,y);
 			}
